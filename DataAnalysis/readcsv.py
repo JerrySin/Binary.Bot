85ee994688eb4bc4be6e1d12f5abbe746a7a4f58
jerry xian <jerry.xian@mobilelive.ca>
2017-03-20 23:25:34 -0400

    New data

--- DataAnalysis/readcsv.py
+++ DataAnalysis/readcsv.py
@@ -1,7 +1,33 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def printData():
-    df = pd.read_csv("Volatility_100_Index_(1t)_20170317_111837.csv")
-    print(df)
+    df = pd.read_csv("Volatility 100 Index (1t).csv")
+    print df.head(10)
 
-printData()
+    print "\nMax Index:"
+    print get_max_index(df)
+    print "\nMin Index:"
+    print get_min_index(df)
+    print "\nDifference:"
+    print get_diff_index(df)
+    print "\nMean Index:"
+    print get_mean_index(df)
+
+    df['Volatility 100 Index'].plot()
+    plt.show()
+
+def get_max_index(df1):
+    return df1['Volatility 100 Index'].max()
+
+def get_min_index(df1):
+    return df1['Volatility 100 Index'].min()
+
+def get_diff_index(df1):
+    return abs(df1['Volatility 100 Index'].max()-df1['Volatility 100 Index'].min())
+
+def get_mean_index(df1):
+    return df1['Volatility 100 Index'].mean()
+
+if __name__ == "__main__":
+    printData()
